c47ea6adf0f41842688a131d20daf9fc491b8485
Anastas Laskov <anastas.luskov@abv.bg>
2018-04-03 01:14:48 +0300

    templating lab added

--- jsTemplating/app.js
+++ jsTemplating/app.js
@@ -1,3 +1,36 @@
 $(() => {
-    // TODO
+    let details;
+    let data;
+    async function loadFiles() {
+        let contacts = await $.get('templates/contacts.hbs');
+        let contactsTemplate = Handlebars.compile(contacts);
+
+        details = await $.get('templates/details.hbs');
+        data = await $.get('data.json');
+        let obj = {
+            contacts: data
+        };
+
+        let table = contactsTemplate(obj);
+        $('#list').append(table);
+        attachEvents();
+    }
+
+    loadFiles();
+
+    function attachEvents() {
+        $('.contact').click(function () {
+            loadDetails($(this).attr('data-id'));
+            $('.contact').removeClass('active');
+            $(this).addClass('active');
+        });
+    }
+
+    function loadDetails(id) {
+        let detailsTemplate = Handlebars.compile(details);
+        let html = detailsTemplate(data[id]);
+        $('#details').empty();
+        $('#details').append(html);
+    }
+
 });
